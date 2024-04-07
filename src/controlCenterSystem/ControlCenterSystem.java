4afad6697c84f66c88b2a68a5d22051a13fd0bb5
Javier <notri80@gmail.com>
2024-04-07 16:25:32 +0200

    Add method addTrafficControlSystem to Control Center System class. Method adds a new Traffic Control System to the list

--- src/controlCenterSystem/ControlCenterSystem.java
+++ src/controlCenterSystem/ControlCenterSystem.java
@@ -3,19 +3,38 @@
  */
 package controlCenterSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import trafficControlSystem.TrafficControlSystem;
+
 /**
  * 
  */
 public class ControlCenterSystem {
 	
-	private int systemID;
-	
+	// vars
+	private int systemID = 700;
+	private List<TrafficControlSystem> listOfTrafficControlSSystems;
 	/** Default constructor */
 	public ControlCenterSystem() {
-		
+		this.systemID++;
+		this.listOfTrafficControlSSystems =  new ArrayList<>();
 	}
 	
 
+
+	/***
+	 * Method adds a new Traffic Control System to the list 
+	 * of Traffic Control Systems that this Control Centre System manages
+	 * 
+	 */
+	private void addTrafficContolSystem() {
+		
+		TrafficControlSystem newTCS = new TrafficControlSystem();
+		listOfTrafficControlSSystems.add(newTCS);
+	}
+	
 	/**
 	 * @return the systemID
 	 */
